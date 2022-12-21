dc7af1d967a4ea856dba20cb676b5883fe2bfd26
Dave Horton <daveh@beachdognet.com>
2022-12-21 09:41:04 -0500

    add ability to add in ranges to be blocked

--- README.md
+++ README.md
@@ -17,3 +17,24 @@ fetched 2 ips, next ID 1645195595
 
 Success! The redis set named my-blacklist now contains 502 ips
 ```
+# Adding additional ranges
+
+You can also provide a list of network CIDRs that you want to block in addition to whatever apiban is currently blocking:
+
+```bash
+$ APIBAN_REDIS_SERVER=127.0.0.1 APIBAN_REDIS_PORT=6379 \
+APIBAN_REDIS_KEY=my-blacklist \
+APIBAN_API_KEY=your-apiban-key \
+APIBAN_FOREVER_BLOCK_RANGES="128.90.0.0/16, 162.142.125.0/24, 198.235.24.0/24" \
+apiban-redis
+
+adding 65536 ips from 128.90.0.0/16 to forever block list
+adding 256 ips from  162.142.125.0/24 to forever block list
+adding 256 ips from  198.235.24.0/24 to forever block list
+fetched 250 ips, next ID 1671198826
+fetched 250 ips, next ID 1671445397
+fetched 250 ips, next ID 1671583586
+fetched 92 ips, next ID 1671632041
+
+Success! the redis set named my-blacklist now contains 66890 ips
+```
--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 const { createClient } = require('redis');
 const bent = require('bent');
 const get = bent('https://apiban.org', 'GET', 'json', 200);
+const foreverBlockRanges = [];
 let client;
 
 /* validate environment */
@@ -11,10 +12,34 @@ const {
   APIBAN_REDIS_SERVER,
   APIBAN_REDIS_PORT = 6379,
   APIBAN_REDIS_KEY,
+  APIBAN_FOREVER_BLOCK_RANGES
 } = process.env;
 assert.ok(APIBAN_REDIS_SERVER, 'env APIBAN_REDIS_SERVER is required');
 assert.ok(APIBAN_REDIS_KEY, 'env APIBAN_REDIS_KEY is required');
 assert.ok(APIBAN_API_KEY, 'env APIBAN_API_KEY is required');
+
+const cidrToIpRange = (cidr) => {
+  const [ip, subnet] = cidr.split('/');
+  const subnetInt = parseInt(subnet);
+  const mask = ~(Math.pow(2, 32 - subnetInt) - 1);
+  const ipInt = (ip.split('.').map(Number).reduce((acc, octet) => (acc << 8) + octet, 0)) >>> 0;
+  const startInt = ipInt & mask;
+  const endInt = startInt + ~mask;
+
+  const range = [];
+  for (let i = startInt; i <= endInt; i++) {
+    range.push((i >>> 24) + '.' + ((i >>> 16) & 255) + '.' + ((i >>> 8) & 255) + '.' + (i & 255));
+  }
+  return range;
+};
+
+if (APIBAN_FOREVER_BLOCK_RANGES) {
+  APIBAN_FOREVER_BLOCK_RANGES.split(',').forEach((range) => {
+    const arr = cidrToIpRange(range);
+    console.log(`adding ${arr.length} ips from ${range} to forever block list`);
+    foreverBlockRanges.push(...cidrToIpRange(range));
+  });
+}
 
 //const buff = Buffer.from(`${APIBAN_USERNAME}:${APIBAN_PASSWORD}`);
 //const base64data = buff.toString('base64');
@@ -69,10 +94,11 @@ const getBlacklist = async() => {
 
 const updateRedis = async(ips) => {
   try {
+    const arr = [...ips, ...foreverBlockRanges];
     await client.DEL(APIBAN_REDIS_KEY);
-    await client.sendCommand(['SADD', APIBAN_REDIS_KEY, ...ips]);
+    await client.sendCommand(['SADD', APIBAN_REDIS_KEY, ...arr]);
     console.log();
-    console.log(`Success! the redis set named ${APIBAN_REDIS_KEY} now contains ${ips.length} ips`);
+    console.log(`Success! the redis set named ${APIBAN_REDIS_KEY} now contains ${arr.length} ips`);
   } catch (err) {
     console.log(err);
   }
